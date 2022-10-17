05b5888208513cf0ad43ccee6bb5b460b20bba70
mariusbij <mariusvolovnik@gmail.com>
2022-10-17 03:27:43 +0300

    init

--- src/QualityUpdaters/QualityUpdaterFactory.php
+++ src/QualityUpdaters/QualityUpdaterFactory.php
@@ -13,7 +13,7 @@ class QualityUpdaterFactory
             'Backstage passes to a TAFKAL80ETC concert' => new BackstagePass(),
             'Sulfuras, Hand of Ragnaros' => new Sulfuras(),
             'Conjured Mana Cake' => new Conjured(),
-            default => new Normal(),
+            default => new Normal()
         };
     }
 }
